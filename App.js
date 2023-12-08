f4b24f0d6d3d073cd6f3af581c0cfe1337a3c55b
Daniel Tovar <91617620+Danny112T@users.noreply.github.com>
2023-12-08 10:20:25 -0600

    ➕ Add CORS and Morgan middleware

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const routerUser = require("./src/routes/users.routes");
 const routerTask = require("./src/routes/tasks.routes");
 const app = express();
 const port = process.env.PORT;
 const host = process.env.HOST;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"));
 
 app.use("/users", routerUser);
 app.use("/tasks", routerTask);
+
+// Middleware de registro personalizado
+app.use((req, res, next) => {
+    const fechaActual = new Date().toISOString();
+    const metodo = req.method;
+    const url = req.url;
+    const ipCliente = req.ip || req.connection.remoteAddress;
+
+    console.log(`[${fechaActual}] ${metodo} ${url} - IP: ${ipCliente}`);
+    next();
+});
 
 app.listen(port, () => {
     console.log(`Server is running on: ${host}:${port}`);
