e85d85b6975c36d242752baa8ede9e76cee0db56
Daniel Tovar <91617620+Danny112T@users.noreply.github.com>
2023-12-22 19:35:36 -0600

    ⚡️ Cambios para Dockerizar la App

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,20 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const uriLocal = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+const uriRemota = process.env.DB_URI;
 
-mongoose.connect(uriLocal);
+mongoose.connect(uriRemota || uriLocal, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 const db = mongoose.connection;
+
+db.on(
+    "error",
+    console.error.bind(console, "Error de conexión a la base de datos:")
+);
+db.once("open", () => {
+    console.log("Conexión exitosa a la base de datos");
+});
 
 module.exports = mongoose;
