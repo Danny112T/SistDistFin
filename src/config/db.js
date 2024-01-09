5876f95d1b1ad7077cd22cfea6ac4c70431b096e
Daniel Tovar <91617620+Danny112T@users.noreply.github.com>
2024-01-08 23:27:33 -0600

    🚀 Update to add a container for the DB

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 const uriLocal = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const uriRemota = process.env.DB_URI;
 
-mongoose.connect(uriRemota || uriLocal, {
+mongoose.connect(uriLocal || uriRemota, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}).then(()=>console.log("Conexión exitosa a la base de datos :D")).catch((err)=>console.log("Error: " + err));
 const db = mongoose.connection;
 
 db.on(
